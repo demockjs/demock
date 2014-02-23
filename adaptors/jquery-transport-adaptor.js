0718a3d1e56a0b33661b89f00c033490b0bf27b9
Ates Goral <ates@magnetiq.com>
2014-02-23 05:18:01 -0500

    Externalized jQuery transport adaptor

---
+++ adaptors/jquery-transport-adaptor.js
@@ -0,0 +1,74 @@
+/*$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+    var request = {
+        method: options.type.toUpperCase(),
+        url: originalOptions.url,
+        params: originalOptions.data
+    };
+
+    indexJson.filterRequest(request);
+
+    options.type = request.method;
+    options.url = request.url;
+    options.data = request.params;
+
+    jqXHR.__request = request;
+});*/
+
+$.ajax = (function (ajax) {
+    return function (settings) {
+        var request = {
+            method: settings.type ? settings.type.toUpperCase() : 'GET',
+            url: settings.url,
+            params: settings.data
+        };
+
+        indexJson.filterRequest(request);
+
+        settings.type = request.method;
+        settings.url = request.url;
+        settings.data = request.params;
+
+        return ajax.call(this, settings).then(function (data, textStatus, jqXHR) {
+            var response = {
+                    //delay: 0,
+                    //timeout: false,
+                    statusCode: jqXHR.status,
+                    statusText: jqXHR.statusText,
+                    data: data
+                },
+                fakeXHR = $.Deferred();
+
+            while (indexJson.filterResponse(request, response)) {}
+
+            function resolve() {
+                // @todo massage/fake jqXHR
+                // textStatus: "error", "abort", "parsererror"                    
+                if (response.timeout) {
+                    fakeXHR.reject(jqXHR, 'timeout');
+                } else if (response.statusCode >= 400 && response.statusCode < 600) {
+                    fakeXHR.reject(jqXHR, 'error', response.statusText);
+                } else {
+                    fakeXHR.resolve(response.data, textStatus, jqXHR);
+                }
+            }
+
+            if (response.delay) {
+                var timeout = window.setTimeout(resolve, response.delay);
+
+                return fakeXHR.promise({
+                    abort: function () {
+                        if (timeout) {
+                            window.clearTimeout(timeout);
+                        }
+                    }
+                });
+            } else {
+                resolve();
+
+                return fakeXHR.promise({
+                    abort: $.noop
+                });
+            }
+        });
+    };
+}($.ajax));
--- index.json.js
+++ index.json.js
@@ -1,5 +1,5 @@
-// Based on https://github.com/umdjs/umd/blob/master/commonjsStrictGlobal.js
 (function (root, factory) {
+    // Based on https://github.com/umdjs/umd/blob/master/commonjsStrictGlobal.js
     if (typeof define === 'function' && define.amd) {
         // AMD. Register as an anonymous module.
         define([ 'exports' ], function (exports) {
@@ -19,6 +19,7 @@
         method: function (request) {
             if (request.method !== 'GET') {
                 request.url = request.url + request.method;
+                request.method = 'GET';
             }
         }
     };
@@ -27,7 +28,7 @@
         delay: function (request, response, delay) {
             response.delay = delay;
         },
-        status: function (request, response, status) {
+        status: function (request, response, status) { // @todo statusCode & statusText?
             response.statusCode = status.code || response.statusCode;
             response.statusText = status.text || response.statusText;
         },
@@ -35,15 +36,30 @@
         timeout: function (request, response) {
             response.timeout = true;
         }
-        // switch
+        // switch: function (request, response, paramName) {
+        //     var cases = response.data[exports.filterPrefix + 'case'],
+        //         paramValue = request.params && request.params[paramName];
+
+        //     if (cases && cases.hasOwnProperty(paramValue)) {
+        //         console.log(cases[paramValue]);
+        //         response.data = cases[paramValue];
+        //     }
+        // }
+        //parseerror: "msg" -> errormsg
         // random
         // list [ 1, 3, 4 ] -- fetch individual entities    
+    };
+
+    exports.filterRequest = function (request) {
+        for (var filterName in exports.requestFilters) {
+            exports.requestFilters[filterName](request);
+        }
     };
 
     /**
      * @return true when any filtering was done
      */
-    exports.filter = function (request, response) {
+    exports.filterResponse = function (request, response) {
         var filtered = false;
 
         function applyFilter(filterName, filterFn) {
