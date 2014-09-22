07575a503c7e44cc5126d54ff55a0252bf46072a
Ates Goral <ates.g@myplanetdigital.com>
2014-09-22 14:52:53 -0400

    Significantly changed API. Partial migration of old tests.

--- test/test-demock.js
+++ test/test-demock.js
@@ -1,9 +1,14 @@
 define([
     'intern!bdd',
+    'intern/chai!',
     'intern/chai!expect',
+    'node_modules/sinon/lib/sinon',
+    'node_modules/sinon-chai/lib/sinon-chai',
     'demock'
-], function (bdd, expect, demock) {
+], function (bdd, chai, expect, sinon, sinonChai, Demock) {
     'use strict';
+
+    chai.use(sinonChai);
 
     var describe = bdd.describe,
         before = bdd.before,
@@ -12,13 +17,11 @@ define([
         afterEach = bdd.after,
         it = bdd.it;
 
-    describe('demock', function () {
+    describe('Demock', function () {
+        var demock;
 
-        describe('#filterPrefix', function () {
-
-            it('should be set to $ by default', function () {
-                expect(demock.filterPrefix).to.equal('$');
-            });
+        beforeEach(function () {
+            demock = new Demock();
         });
 
         describe('#config', function () {
@@ -26,187 +29,264 @@ define([
             it('should be an object', function () {
                 expect(demock.config).to.be.an('object');
             });
+
+            describe('filterPrefix', function () {
+
+                it('should be set to $ by default', function () {
+                    expect(demock.config.filterPrefix).to.equal('$');
+                });
+            });
         });
 
         describe('#filterRequest()', function () {
-            var defaultFilters;
 
-            before(function() {
-                defaultFilters = demock.requestFilters;
-            });
+            describe('with no filters', function () {
 
-            after(function () {
-                demock.requestFilters = defaultFilters;
-            });
-        });
-    });
-
-    describe('default request filters', function () {
-
-        describe('method filter', function () {
-
-            describe('with a GET request', function () {
-
-                it('should leave the method and the URL untouched', function () {
+                it('should leave the request object untouched', function () {
                     var request = {
                         method: 'GET',
-                        url: '/api/test'
+                        url: '/api/test',
+                        params: { a: 1 }
                     };
 
-                    demock.requestFilters.method(request);
+                    demock.filterRequest(request);
 
                     expect(request.method).to.equal('GET');
                     expect(request.url).to.equal('/api/test');
+                    expect(request.params).to.deep.equal({ a: 1 });
                 });
             });
 
-            describe('with a non-GET request', function () {
+            describe('with a filter', function () {
 
-                it('should change the method to GET', function () {
+                it('should ignore the filter if the filter does not have a filterRequest method', function () {
+                    demock.use({});
+
                     var request = {
-                        method: 'NONGET',
-                        url: '/api/test'
+                        method: 'GET',
+                        url: '/api/test',
+                        params: { a: 1 }
                     };
 
-                    demock.requestFilters.method(request);
+                    demock.filterRequest(request);
 
                     expect(request.method).to.equal('GET');
+                    expect(request.url).to.equal('/api/test');
+                    expect(request.params).to.deep.equal({ a: 1 });
                 });
 
-                describe('with URL with no trailing /', function () {
+                it('should pass the request object to the filterRequest method of the filter', function () {
+                    var filter = {
+                        filterRequest: sinon.spy()
+                    };
 
-                    it('should append the method name as subfolder to the URL path', function () {
+                    demock.use(filter);
+
+                    var request = {
+                        method: 'GET',
+                        url: '/api/test',
+                        params: { a: 1 }
+                    };
+
+                    demock.filterRequest(request);
+
+                    expect(filter.filterRequest).to.have.been.calledWith(request);
+                });
+            });
+        });
+
+        describe('#filterResponse()', function () {
+
+            describe('with no filters', function () {
+
+                it('should leave the response object untouched', function () {
+                    var response = {
+                        statusCode: 200,
+                        statusText: 'OK',
+                        data: { a: 1 }
+                    };
+
+                    demock.filterResponse({}, response);
+
+                    expect(response.statusCode).to.equal(200);
+                    expect(response.statusText).to.equal('OK');
+                    expect(response.data).to.deep.equal({ a: 1 });
+                });
+            });
+        });
+
+        describe('stock request filters', function () {
+
+            describe('method filter', function () {
+
+                beforeEach(function () {
+                    demock.use(demock.method());
+                });
+
+                describe('with a GET request', function () {
+
+                    it('should leave the method and the URL untouched', function () {
                         var request = {
-                            method: 'NONGET',
+                            method: 'GET',
                             url: '/api/test'
                         };
 
-                        demock.requestFilters.method(request);
+                        demock.filterRequest(request);
 
-                        expect(request.url).to.equal('/api/test/NONGET/');
-                    });
-                });
-
-                describe('with URL with trailing /', function () {
-
-                    it('should append the method name as subfolder to the URL path', function () {
-                        var request = {
-                            method: 'NONGET',
-                            url: '/api/test/'
-                        };
-
-                        demock.requestFilters.method(request);
-
-                        expect(request.url).to.equal('/api/test/NONGET/');
+                        expect(request.method).to.equal('GET');
+                        expect(request.url).to.equal('/api/test');
                     });
                 });
             });
         });
 
-        describe('defaultDocument filter', function () {
-            beforeEach(function () {
-                demock.config.defaultDocument = 'index.json';
-            });
+    //         describe('with a non-GET request', function () {
 
-            afterEach(function () {
-                demock.config.defaultDocument = '';
-            });
+    //             it('should change the method to GET', function () {
+    //                 var request = {
+    //                     method: 'NONGET',
+    //                     url: '/api/test'
+    //                 };
 
-            describe('with URL with no trailing /', function () {
+    //                 demock.requestFilters.method(request);
 
-                it('should append the configured default document filename to the URL path', function () {
-                    var request = {
-                        method: 'GET',
-                        url: '/api/test'
-                    };
+    //                 expect(request.method).to.equal('GET');
+    //             });
 
-                    demock.requestFilters.defaultDocument(request);
+    //             describe('with URL with no trailing /', function () {
 
-                    expect(request.url).to.equal('/api/test/index.json');
-                });
-            });
+    //                 it('should append the method name as subfolder to the URL path', function () {
+    //                     var request = {
+    //                         method: 'NONGET',
+    //                         url: '/api/test'
+    //                     };
 
-            describe('with URL with trailing /', function () {
+    //                     demock.requestFilters.method(request);
 
-                it('should append the configured default document filename to the URL path', function () {
-                    var request = {
-                        method: 'GET',
-                        url: '/api/test/'
-                    };
+    //                     expect(request.url).to.equal('/api/test/NONGET/');
+    //                 });
+    //             });
 
-                    demock.requestFilters.defaultDocument(request);
+    //             describe('with URL with trailing /', function () {
 
-                    expect(request.url).to.equal('/api/test/index.json');
-                });
-            });
-        });
+    //                 it('should append the method name as subfolder to the URL path', function () {
+    //                     var request = {
+    //                         method: 'NONGET',
+    //                         url: '/api/test/'
+    //                     };
+
+    //                     demock.requestFilters.method(request);
+
+    //                     expect(request.url).to.equal('/api/test/NONGET/');
+    //                 });
+    //             });
+    //         });
+    //     });
+
+    //     describe('defaultDocument filter', function () {
+    //         beforeEach(function () {
+    //             demock.config.defaultDocument = 'index.json';
+    //         });
+
+    //         afterEach(function () {
+    //             demock.config.defaultDocument = '';
+    //         });
+
+    //         describe('with URL with no trailing /', function () {
+
+    //             it('should append the configured default document filename to the URL path', function () {
+    //                 var request = {
+    //                     method: 'GET',
+    //                     url: '/api/test'
+    //                 };
+
+    //                 demock.requestFilters.defaultDocument(request);
+
+    //                 expect(request.url).to.equal('/api/test/index.json');
+    //             });
+    //         });
+
+    //         describe('with URL with trailing /', function () {
+
+    //             it('should append the configured default document filename to the URL path', function () {
+    //                 var request = {
+    //                     method: 'GET',
+    //                     url: '/api/test/'
+    //                 };
+
+    //                 demock.requestFilters.defaultDocument(request);
+
+    //                 expect(request.url).to.equal('/api/test/index.json');
+    //             });
+    //         });
+    //     });
     });
 
-    describe('default response filters', function () {
+    // describe('stock response filters', function () {
 
-        describe('data filter', function () {
+    //     describe('data filter', function () {
 
-            it('should replace the response payload with the argument', function () {
-                var replacement = {};
+    //         it('should replace the response payload with the argument', function () {
+    //             var replacement = {};
 
-                var response = {
-                    data: {}
-                };
+    //             var response = {
+    //                 data: {}
+    //             };
 
-                demock.responseFilters.data({}, response, replacement);
+    //             demock.responseFilters.data({}, response, replacement);
 
-                expect(response.data).to.equal(replacement);
-            });
-        });
+    //             expect(response.data).to.equal(replacement);
+    //         });
+    //     });
 
-        describe('delay filter', function () {
+    //     describe('delay filter', function () {
 
-        });
+    //     });
 
-        describe('status filter', function () {
+    //     describe('status filter', function () {
 
-            describe('when argument is an object', function () {
+    //         describe('when argument is an object', function () {
 
-                describe('the code property', function () {
+    //             describe('the code property', function () {
 
-                    it('should override the HTTP response status code', function () {
+    //                 it('should override the HTTP response status code', function () {
 
-                        var response = {
-                            statusCode: 200,
-                            statusText: 'OK'
-                        };
+    //                     var response = {
+    //                         statusCode: 200,
+    //                         statusText: 'OK'
+    //                     };
 
-                        demock.responseFilters.status({}, response, { code: 400 });
+    //                     demock.responseFilters.status({}, response, { code: 400 });
 
-                        expect(response.statusCode).to.equal(400);
-                        expect(response.statusText).to.equal('OK');
-                    });
-                });
+    //                     expect(response.statusCode).to.equal(400);
+    //                     expect(response.statusText).to.equal('OK');
+    //                 });
+    //             });
 
-                describe('the text property', function () {
+    //             describe('the text property', function () {
 
-                    it('should override the HTTP response status text', function () {
+    //                 it('should override the HTTP response status text', function () {
 
-                        var response = {
-                            statusCode: 200,
-                            statusText: 'OK'
-                        };
+    //                     var response = {
+    //                         statusCode: 200,
+    //                         statusText: 'OK'
+    //                     };
 
-                        demock.responseFilters.status({}, response, { text: 'Done' });
+    //                     demock.responseFilters.status({}, response, { text: 'Done' });
 
-                        expect(response.statusCode).to.equal(200);
-                        expect(response.statusText).to.equal('Done');
-                    });
-                });
-            });
-        });
+    //                     expect(response.statusCode).to.equal(200);
+    //                     expect(response.statusText).to.equal('Done');
+    //                 });
+    //             });
+    //         });
+    //     });
 
-        describe('timeout filter', function () {
+    //     describe('timeout filter', function () {
 
-        });
+    //     });
 
-        describe('switch filter', function () {
+    //     describe('switch filter', function () {
 
-        });
-    });
+    //     });
+    // });
 });
